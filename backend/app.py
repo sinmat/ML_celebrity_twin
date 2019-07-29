26a3c18bcc346b56c32da34d79e304d47c6867ef
Sonja Matejina <sonja.matejina@gmail.com>
2019-07-29 15:39:50 -0700

    updates

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,15 @@
 import json
 import os
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask,url_for,request,render_template,jsonify,send_file
 from flask_cors import CORS
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = '../ml/faces/train'
-ALLOWED_EXTENSIONS = set(['txt','jpg'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 CORS(app)
 
 @app.route('/')
 def index():
-    return 'Hello World!!!'
+	return render_template('index.js')
 
 # POST image and return a match JSON dict
 @app.route('/match',methods=['GET','POST'])
@@ -35,33 +28,33 @@ def match():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "Image Saved!"
 
-   
-    # Save image to /tmp and assign it's path to a variable
 
-    # Call KNN predict method
+
+#     # Call KNN predict method
+
     
-    # Assign data to result_dict
-    result_dict['hello'] = "world!"  
+#     # Assign data to result_dict
+#     result_dict['hello'] = "world!"  
 
-    #* Call SVM predict method
-    #* Assign data to result_dict
 
-    # Remove tmp image to preserve space
+#     # Remove tmp image to preserve space
     
-    # Encode result_dict as JSON and return back to user
-    return jsonify(result_dict)
+#     # Encode result_dict as JSON and return back to user
+#     return jsonify(result_dict)
 
-## Define KNN predict model function that takes in image as parameter and 
-## returns back relevant data as dict
-def knn_predict(img_path = ""):
-    result_dict = {}
+# ## Define KNN predict model function that takes in image as parameter and 
+# @app.route('/predict',methods=['GET','POST'])
 
-    # Run prediction
+# ## returns back relevant data as dict
+# def knn_predict(img_path = ""):
+#     result_dict = {}
+
+#     # Run prediction
     
-    # Assign data to result_dict
-    result_dict['hello'] = "world!" 
+#     # Assign data to result_dict
+#     result_dict['predict'] = "world!" 
 
-    return result_dict
+#     return result_dict
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
